eb98ad4e729930f69256a1e3e962f6f5c4aec16d
Joel Schwegmann <schwegmann@hob-by-horse.de>
2025-07-09 19:06:55 +0200

    update frontend

---
+++ src/components/MemberCard.vue
@@ -0,0 +1,80 @@
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    // type inference enabled
+    name: "MemberCard",
+    props: {
+        member: Object,
+    },
+});
+
+</script>
+
+<template>
+<div class="membercard_item">
+    <img :src="member?.image" alt="name">
+    <div class="membercard_item__right">
+        <h2 class="membercard_item__right__name">{{member.name}}</h2>
+        <div class="membercard_item__actions">
+            <h2 v-html="member.count"></h2>
+            <div class="membercard_item__actions__buttons">
+                <button class="membercard_item__actions__buttons__plus">+</button>
+                <button class="membercard_item__actions__buttons__minus">-</button>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<style lang="scss">
+h2 {
+    margin: 0
+}
+.membercard_item {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: .5rem;
+    padding: .75rem;
+    background-color: #e9edf5;
+    border-radius: 8px;
+    box-shadow: 1px 2px 8px #ccc;
+    border: 1px solid #fff;
+    &__right {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        gap: .25rem;
+    }
+    &__actions {
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        align-items: center;
+        &__buttons {
+            display: flex;
+            flex-direction: row;
+            column-gap: .5rem;
+            button {
+                height: 3rem;
+                aspect-ratio: 1;
+                width: auto;
+                font-size: 2rem;
+                border-radius: .25rem;
+                color: #fff;
+            }
+            &__plus {
+                background: linear-gradient(180deg, #49da17, #19ab0e);
+                border: 2px solid #49da17;
+            }
+            &__minus {
+                background: linear-gradient(180deg, #da1734, #a9162c);
+                border: 2px solid #da1734;
+            }
+        }
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,50 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
-</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import MemberCard from '@/components/MemberCard.vue';
 
 export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
+    name: 'HomeView',
+    components: {
+        MemberCard
+    },
+    data() {
+        return {
+            members: [
+                {
+                    name: "Mulle",
+                    image: "",
+                    count: 0,
+                },
+                {
+                    name: "Mahir",
+                    image: "",
+                    count: 0,
+                },
+                {
+                    name: "Simon",
+                    image: "",
+                    count: 0,
+                }
+            ]
+        }
+    }
 });
 </script>
+
+<template>
+  <div class="home">
+      <h1><b>Heilig Geist</b><br>🚬Kippen-Counter🚬</h1>
+      <div class="home__memmber__listing">
+          <MemberCard v-for="member in members" :key="member" :member="member"></MemberCard>
+      </div>
+  </div>
+</template>
+
+<style lang="scss">
+.home__memmber__listing {
+    display: flex;
+    flex-direction: column;
+    gap: .75rem;
+}
+</style>
