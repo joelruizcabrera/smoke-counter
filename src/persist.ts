da9c21b4d954030838f989ea2a16d809444ce2f6
Joel Schwegmann <schwegmann@hob-by-horse.de>
2025-07-09 22:00:52 +0200

    adding redis

--- src/persist.ts
+++ src/persist.ts
@@ -1,3 +1,7 @@
-export function persistData(members:object) {
-    console.log(members)
+import { createClient } from 'redis';
+export async function persistData(members:object) {
+    const redis =  await createClient({ url: process.env.REDIS_URL }).connect();
+    await redis.set('key', 'value');
+    const value = await redis.get('key');
+    console.log(value)
 }
