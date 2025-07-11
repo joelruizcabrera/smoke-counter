9b8217a59c4f12f1b1bf484d510702e0b07a2ca5
Joel Schwegmann <schwegmann@hob-by-horse.de>
2025-07-11 21:31:49 +0200

    styling step input field

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -31,12 +31,12 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="home">
+  <div class="home" style="padding-bottom: 5rem">
       <h1><b>Heilig Geist</b><br>🚬Kippen-Counter🚬</h1>
       <div class="home__memmber__listing">
           <MemberCard v-for="member in members" :key="member" :member="member" :steps="steps"></MemberCard>
       </div>
-      <input type="number" v-model="steps">
+      <input type="number" v-model="steps" class="step_input">
   </div>
 </template>
 
@@ -45,5 +45,19 @@ export default defineComponent({
     display: flex;
     flex-direction: column;
     gap: .75rem;
+}
+
+.step_input {
+    position: fixed;
+    bottom: 2rem;
+    left: 0;
+    right: 0;
+    max-width: 10rem;
+    margin: 0 auto;
+    z-index: 1;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: .5rem;
+    box-shadow: 0px 0px 13px 10px #fff;
 }
 </style>
